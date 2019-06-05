1251397a80a988c27e40f98b75c16423361c5584
wannerdev <7975579+wannerdev@users.noreply.github.com>
2019-06-05 18:01:45 +0200

    First commit basic moodle client.

---
+++ src/de/HTW/Main2.java
@@ -0,0 +1,25 @@
+package de.HTW;
+
+import lenz.htw.cywwtaip.net.NetworkClient;
+import lenz.htw.cywwtaip.world.GraphNode;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        NetworkClient client = new NetworkClient(null, "ICH", "SUPER!");
+
+        while (client.isAlive()) {
+            client.getBotSpeed(0); // raw constant
+            client.getScore(client.getMyPlayerNumber());
+            client.changeMoveDirection(1, -0.08f);
+
+            float[] position = client.getBotPosition(0, 0); // array with x,y,z
+            float[] direction = client.getBotDirection(0); // array with x,y,z
+
+            GraphNode[] graph = client.getGraph();
+            for (GraphNode n : graph[0].neighbors) {
+                System.out.println(n + ": " + n.owner + ", " + n.blocked);
+            }
+        }
+    }
+}
