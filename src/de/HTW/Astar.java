5fa36b01c774c1c4fe6c4d6f755599be27151823
wannerdev <7975579+wannerdev@users.noreply.github.com>
2019-06-20 11:21:33 +0200

    added functions to get get node id for bots + Astar start

---
+++ src/de/HTW/Astar.java
@@ -0,0 +1,4 @@
+package de.HTW;
+
+public class Astar {
+}
--- src/de/HTW/Client.java
+++ src/de/HTW/Client.java
@@ -6,15 +6,18 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 public class Client {
 
+    private static float nodesOfBots[];
+
     // java -Djava.library.path=lib/native -jar cywwtaip.jar
     public static void main(String[] args) {
-        NetworkClient client = new NetworkClient("localhost", " Torsten", "SUPER!");
+        NetworkClient client = new NetworkClient("localhost", args[0]+" Torsten", "SUPER!");
         Rotator rot = new Rotator();
         while (client.isAlive()) {
             client.getBotSpeed(0); // raw constant
             client.getScore(client.getMyPlayerNumber());
             GraphNode[] myGraph = client.getGraph();
-            int nodeNo = 0 ;
+            int nodeNo = 0;
+            nodesOfBots = new float[]{-1,-1,-1};
             Vector3D Destination =  new Vector3D( 0,1,0);//myGraph[nodeNo].x,myGraph[nodeNo].y,myGraph[nodeNo].z);
           //  System.out.println(Destination.toString());
             float ang1 =  rot.getRotationAngle(Destination,client,0, client.getMyPlayerNumber());
@@ -27,11 +30,61 @@ public class Client {
            // float[] position = client.getBotPosition(0, 0); // array with x,y,z
             //float[] direction = client.getBotDirection(0); // array with x,y,z
 
-           /* GraphNode[] graph = client.getGraph();
+            /*GraphNode[] graph = client.getGraph();
             for (GraphNode n : graph[0].neighbors) {
                 System.out.println(n + ": " + n.owner + ", " + n.blocked);
             }*/
 
         }
+    }
+
+    /**
+     * Sets the array nodesOfBots to the GraphNode indexes on the server
+     * @param client
+     */
+    private static void getMyNodes(NetworkClient client){
+        GraphNode[] graph = client.getGraph();
+        float pos1[] = client.getBotPosition(client.getMyPlayerNumber(),0);
+        float pos2[] = client.getBotPosition(client.getMyPlayerNumber(),0);
+        float pos3[] = client.getBotPosition(client.getMyPlayerNumber(),0);
+        for (int i =0 ; i< graph.length; i++) {
+            GraphNode n = graph[i];
+            if(atNode(n,pos1))nodesOfBots[0]=i;
+            if(atNode(n,pos2))nodesOfBots[1]=i;
+            if(atNode(n,pos3))nodesOfBots[2]=i;
+        }
+    }
+
+    /**
+     * Searches graph for the node with the corresponding bot.(dumb search)
+     * @param client
+     * @param bot
+     * @returns GraphNode of the position of the Bot
+     */
+    public static GraphNode getMyNodeId(NetworkClient client, int bot){
+        GraphNode[] graph = client.getGraph();
+        float pos1[] = client.getBotPosition(client.getMyPlayerNumber(),bot);
+        int i=0;
+        for (; i< graph.length; i++) {
+            if(atNode(graph[i],pos1)){
+                nodesOfBots[bot]=i;
+                break;
+            }
+        }
+        return graph[i];
+    }
+
+    /**
+     * Just a simple check if the positions match
+     * @param n
+     * @param spot
+     * @return
+     */
+    public static boolean atNode(GraphNode n, float spot[]){
+        if (n.blocked) return false;
+        if(n.x ==spot[0]&& n.y ==spot[1]&& n.z ==spot[2]) {
+            return true;
+        }
+        return false;
     }
 }
